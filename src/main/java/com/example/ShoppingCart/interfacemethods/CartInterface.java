81d5edbf2520c13f85c403d453768f5053b1dc52
lusiyuan <siyuanlu345@gmail.com>
2025-10-02 12:33:24 +0800

    checkout relevant method in interfaces of cart and order

--- src/main/java/com/example/ShoppingCart/interfacemethods/CartInterface.java
+++ src/main/java/com/example/ShoppingCart/interfacemethods/CartInterface.java
@@ -1,4 +1,13 @@
 package com.example.ShoppingCart.interfacemethods;
 
+import com.example.ShoppingCart.model.CartRecord;
+
+import java.util.List;
+
 public interface CartInterface {
+    //updateCartRecord
+    public CartRecord updateCartItem(String userId, Long productId, Integer quantity);
+
+    //updateCartItem
+    public List<CartRecord> updateCartItemsByUserId(String userId, Long productId, Integer quantity);
 }
--- src/main/java/com/example/ShoppingCart/interfacemethods/OrderInterface.java
+++ src/main/java/com/example/ShoppingCart/interfacemethods/OrderInterface.java
@@ -1,4 +1,28 @@
 package com.example.ShoppingCart.interfacemethods;
 
+import com.example.ShoppingCart.model.Order;
+import com.example.ShoppingCart.model.OrderItem;
+import com.example.ShoppingCart.model.PaymentRecord;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface OrderInterface {
+
+
+    //checkout
+    //2.createOrder
+    public Order createOrder(@RequestBody Order order);
+
+    //calculateTotalAmount
+    public double calculateTotalPrice(@RequestBody Order order);
+
+    //updateOrderStatus
+    public void updateOrderStatus(@RequestBody Order order);
+
+    //createOrderItems
+    public List<OrderItem> createOrderItem(@RequestBody OrderItem orderItem);
+
+    //createPayment
+    public PaymentRecord createPaymentRecord(@RequestBody PaymentRecord paymentRecord);
 }
